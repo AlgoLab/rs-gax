559e9e782bdc2e63180b90d2021f6177675dbd6c
matteo <fiorematteo2002@gmail.com>
2022-11-29 12:13:33 +0100

    Add tests in gamp

--- src/gamp.rs
+++ src/gamp.rs
@@ -12,3 +12,79 @@ pub fn write(
 ) -> Result<(), Error> {
     framing::write::<vg::MultipathAlignment>(alignments, &mut out_file)
 }
+
+#[cfg(test)]
+mod tests {
+    use prost_types::{value::Kind, Value};
+
+    use super::*;
+    use std::fs::File;
+
+    #[test]
+    fn gamp_read() {
+        let in_file = "data/example.gamp";
+        let f = File::open(in_file).unwrap();
+        let alignments: Vec<vg::MultipathAlignment> = parse(f).unwrap();
+        let first = alignments[0].clone();
+
+        let name = "FBtr0342963_e_1536_X_294766";
+        let mapping_quality = 60;
+        let sequence = "TAGATAAAAAATAAACGGAAAATTTGTTATTTCTTTCGTACATGGTAAAGAATCTTTTTTTACTTGTGTTTCTGTGATTTGAGTGTTTGAAAAATTTAAC";
+        let start = vec![0];
+
+        assert_eq!(first.name, name);
+        assert_eq!(first.mapping_quality, mapping_quality);
+        assert_eq!(first.sequence, sequence);
+        assert_eq!(first.start, start);
+    }
+
+    #[test]
+    fn gamp_write() {
+        let out_file = "data/example.out.gamp";
+        let of = File::create(out_file).unwrap();
+        let alignment = vg::MultipathAlignment {
+            name: "test".into(),
+            mapping_quality: 1000,
+            sequence: "AAAAATAAACGG".into(),
+            start: vec![0],
+            ..Default::default()
+        };
+        let alignments: Vec<vg::MultipathAlignment> = vec![alignment.clone()];
+        write(&alignments, of).unwrap();
+
+        let in_file = "data/example.out.gamp";
+        let f = File::open(in_file).unwrap();
+        let alignments: Vec<vg::MultipathAlignment> = parse(f).unwrap();
+        let first = alignments[0].clone();
+
+        assert_eq!(first, alignment);
+    }
+
+    #[test]
+    fn gamp_edit() {
+        let in_file = "data/example.gamp";
+        let f = File::open(in_file).unwrap();
+
+        let alignments: Vec<vg::MultipathAlignment> = parse(f).unwrap();
+        let mut alignment = alignments[0].clone();
+
+        alignment.name = "new_name".into();
+        alignment.annotation.as_mut().unwrap().fields.insert(
+            "new_value".into(),
+            Value {
+                kind: Some(Kind::NumberValue(10.0)),
+            },
+        );
+
+        let out_file = "data/example.out.gamp";
+        let of = File::create(out_file).unwrap();
+        write(&(vec![alignment.clone()]), of).unwrap();
+
+        let in_file = "data/example.out.gamp";
+        let f = File::open(in_file).unwrap();
+        let alignments: Vec<vg::MultipathAlignment> = parse(f).unwrap();
+        let first = alignments[0].clone();
+
+        assert_eq!(first, alignment);
+    }
+}
